09a1b013b4d3e027a72b4c79e0b0c42728da19d0
Alex <newalexis777@gmail.com>
2022-08-14 23:37:57 +0400

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,33 @@
+﻿//Нахождение суммы положительных и отрицательных элементов массива.
+int[] myArray = new int[12];
+Random rand = new Random();
+int i = 0;  
+int y = 0;       
+while (i < myArray.Length)
+{
+    myArray[i] = rand.Next(-9, 11);
+    if(myArray[i] < 0)
+    {
+        y = myArray[i] + y;
+    }
+    i++;
+}
+
+Console.Write("Сумма отрицательных элементов массива == "); 
+Console.Write(y);
+y = 0;
+i = 0;
+while (i < myArray.Length)
+{
+    myArray[i] = rand.Next(-9, 11);
+    if(myArray[i] > 0)
+    {
+        y = myArray[i] + y;
+    }
+    i++;
+}
+Console.Write("Сумма положительных элементов массива == "); 
+Console.Write(y);
+
+
+
