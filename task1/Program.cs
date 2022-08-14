5d574b4051cc46797948cbbd7c5c629573813ec3
Alex <newalexis777@gmail.com>
2022-08-14 20:49:27 +0400

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Массив из 8 элементов в котором значения от [0, 1]
+int[] array = new int[8];
+Random rand = new Random();
+int index = 0;
+
+while (index < 8)
+{
+    array[index] = rand.Next(2);
+    Console.WriteLine(array[index]);
+    index++;
+}
+
