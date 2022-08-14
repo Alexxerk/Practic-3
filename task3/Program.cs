133d9a4b17388fd87717180fa8118073fdd2c8d3
Alex <newalexis777@gmail.com>
2022-08-15 00:41:07 +0400

    task3

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите размер массива: ");
+int x = int.Parse(Console.ReadLine());
+int[] myArray = new int[x];
+Random rand = new Random();
+int i = 0;
+
+Console.WriteLine("Введите минимальное значение массива: ");
+int min = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение массива: ");
+int max = int.Parse(Console.ReadLine());
+
+while (i < myArray.Length)
+{
+    myArray[i] = rand.Next(min, max);
+    Console.WriteLine(myArray[i] + " ");
+    i++;
+}
+
+i = 0;
+
+while (i < myArray.Length)
+{
+    myArray[i] = -myArray[i];
+    i++;
+}
+
+i = 0;
+
+while (i < myArray.Length)
+{
+    Console.WriteLine(myArray[i] + " ");
+    i++;
+}
+ 
